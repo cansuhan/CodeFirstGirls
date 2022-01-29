f936720c09d85ca14b553b0c1a0ad25954849ca5
cansuhan <89645837+cansuhan@users.noreply.github.com>
2022-01-29 15:21:03 +0000

    Add files via upload
    Website first draft

---
+++ JS/home.js
@@ -0,0 +1,60 @@
+
+
+//Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topNav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topNav";
+  }
+}
+
+
+// Images on a slideshow with responsive buttons 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// Automatic slideshow images 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 10000); // Change image every 10 seconds
+}
+
+
+
